90919709f591f201b6923fd7d78b86a6e26fb3ea
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-09 20:39:36 +0200

    Setup react app and basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              {" "}
+              <Route path='/' element={<HomeScreen />}></Route>
+            </Routes>
+          </Container>
+        </main>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Rating.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Rating = ({ value, text, color, totalStars = 5 }) => {
+    const stars =[]
+    for (let countStar = 0; countStar < totalStars; countStar++) {
+        stars.push(
+            <i
+            key={countStar}
+            style={{ color }}
+            className={
+                value - countStar > 0.5
+                    ? "fas fa-star"
+                    : value - countStar === 0.5
+                        ? "fas fa-star-half-alt"
+                        : "far fa-star"
+            }
+            ></i>
+        )
+    }
+  return (
+    <div className="rating">
+        <span>{stars}</span>
+        <span>{text && text}</span>
+    </div>
+  )
+}
+
+export default Rating
