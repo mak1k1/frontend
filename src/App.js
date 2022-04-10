616980c083d44d1a6834aafd73f5dd7d3a83503f
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-10 12:14:00 +0200

    Got the products from backend app with Axios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Routes>
               {" "}
               <Route path='/' element={<HomeScreen />}></Route>
+              <Route path='/product/:id' element={<ProductScreen />}></Route>
             </Routes>
           </Container>
         </main>
