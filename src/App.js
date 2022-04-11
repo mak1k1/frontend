6ea18e894891c02ce6f338ddf7a828650be949ae
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-11 12:40:58 +0200

    implemented user login and user logout feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
+import LoginScreen from "./screens/LoginScreen"
 
 function App() {
   return (
     <div>
       <Router>
         <Header></Header>
-        <main className='py-3'>
+        <main className="py-3">
           <Container>
             <Routes>
               {" "}
-              <Route path='/' element={<HomeScreen />}></Route>
-              <Route path='/product/:id' element={<ProductScreen />}></Route>
-              <Route path='/cart/'>
+              <Route path="/" element={<HomeScreen />}></Route>
+              <Route path="/login/" element={<LoginScreen />}></Route>
+              <Route path="/product/:id" element={<ProductScreen />}></Route>
+              <Route path="/cart/">
                 <Route path=":id" element={<CartScreen />} />
                 <Route path="" element={<CartScreen />} />
               </Route>
@@ -28,7 +30,7 @@ function App() {
         <Footer></Footer>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
