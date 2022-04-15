1a616984b9302bfeed350791558f0f327fcb3727
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-15 18:54:21 +0200

    implemented payment action and reducer

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -2,12 +2,14 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_FAIL,
   ORDER_CREATE_SUCCESS,
-
   ORDER_CREATE_RESET,
-
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_RESET,
 } from "../constants/orderConstants"
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -34,7 +36,10 @@ export const orderCreateReducer = (state = {}, action) => {
   }
 }
 
-export const orderDetailsReducer = (state = {loading: true, orderItems: [], shippingAddress: {}}, action) => {
+export const orderDetailsReducer = (
+  state = { loading: true, orderItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case ORDER_DETAILS_REQUEST:
       return {
@@ -50,6 +55,31 @@ export const orderDetailsReducer = (state = {loading: true, orderItems: [], ship
       return {
         loading: false,
         error: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_PAY_REQUEST:
+      return {
+        loading: true,
+      }
+    case ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      }
+    case ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+
+    case ORDER_PAY_RESET:
+      return {
       }
     default:
       return state
