5903a001c74c314f6dce326b3c7b2d3758d331b5
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-15 18:44:56 +0200

    Implemented OrderScreen for order details

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from "./screens/ProfileScreen"
 import ShippingScreen from "./screens/ShippingScreen"
 import PaymentScreen from "./screens/PaymentScreen"
 import PlaceOrderScreen from "./screens/PlaceOrderScreen"
+import OrderScreen from "./screens/OrderScreen"
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
               <Route path="/shipping/" element={<ShippingScreen />}></Route>
               <Route path="/payment/" element={<PaymentScreen />}></Route>
               <Route path="/placeorder/" element={<PlaceOrderScreen />}></Route>
+              <Route path="/order/:id" element={<OrderScreen />}></Route>
               <Route path="/product/:id" element={<ProductScreen />}></Route>
               <Route path="/cart/">
                 <Route path=":id" element={<CartScreen />} />
