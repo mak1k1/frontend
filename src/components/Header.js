e4ba9ab953bb87ae7c5edef6fd61184f93d30a3e
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-04-18 19:10:07 +0200

    Added Hashrouter

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <SearchBox />
-            <Nav className="mr-auto">
+            <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>Cart
